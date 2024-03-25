425a9a510ba80cd4690bd12b71e845fc4f977f53
Jose Chavez <jose.chavez@dialexa.com>
2024-03-25 14:26:08 -0700

    Added bank-common library

---
+++ bank-common/src/main/java/com/rangerforce/bankaccount/common/BankAccountCommonLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.rangerforce.bankaccount.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountCommonLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountCommonLibraryApplication.class, args);
+	}
+
+}
---
+++ bank-common/src/test/java/com/rangerforce/bankaccount/common/BankAccountCommonLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangerforce.bankaccount.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountCommonLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
